7d688a4e2ba1b1214287ab90dfeca1c420996036
Alexander Goussas <goussasalexander@gmail.com>
2025-03-26 21:03:16 -0500

    add app skeleton for streaming app in db sink

---
+++ DbSink/app/src/main/java/io/github/tracingperformancelabs/dupe/Configuration.java
@@ -0,0 +1,24 @@
+package io.github.tracingperformancelabs.dupe;
+
+import java.util.Properties;
+
+public class Configuration {
+    private static Configuration INSTANCE;
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    private Properties properties;
+
+    private Configuration() {
+        this.properties = new Properties();
+    }
+
+    public static Configuration the() {
+        if (INSTANCE == null) {
+            INSTANCE = new Configuration();
+        }
+        return INSTANCE;
+    }
+}
